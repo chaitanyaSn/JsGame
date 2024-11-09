f4ef6dd9e8d4b1d958c868b80c8fbab227a788e7
chaitanya <newarechaitanya832@gmail.com>
2024-11-09 22:06:43 +0530

    completed

--- MemoryGame/app.js
+++ MemoryGame/app.js
@@ -53,7 +53,10 @@ const cardArray=[
 cardArray.sort(()=>0.5-Math.random())
 
 const grid=document.querySelector('#grid')
-const cardChoosen=[]
+let cardChoosen=[]
+let cardChoosenId=[]
+let cardWon=[]
+
 function createBoard(){
    for(let i=0;i<cardArray.length;i++){
     const card=document.createElement('img')
@@ -67,11 +70,34 @@ function createBoard(){
 
 createBoard()
 
+function ckeckMatch(){
+    const cards=document.querySelectorAll('img')
+    if(cardChoosen[0]==cardChoosen[1]){
+        cards[cardChoosenId[0]].setAttribute('src','images/white.png')
+        cards[cardChoosenId[1]].setAttribute('src','images/white.png')
+        cards[cardChoosenId[0]].removeEventListener('click',flipCard)
+        cards[cardChoosenId[1]].removeEventListener('click',flipCard)
+        cardWon.push(cardChoosen)
+    }else{
+        cards[cardChoosenId[0]].setAttribute('src','images/blank.png')
+        cards[cardChoosenId[1]].setAttribute('src','images/blank.png')
+    }
+    document.getElementById('result').innerHTML=cardWon.length
+    cardChoosen=[]
+    cardChoosenId=[]
+    if(cardWon.length==cardArray.length/2){
+        document.getElementById('result').innerHTML='Congratulation'
+    }
+}
 
 function flipCard(){
     const cardID=this.getAttribute('data-id')
     const name=cardArray[cardID].name
     cardChoosen.push(name)
+    cardChoosenId.push(cardID)
     this.setAttribute('src',cardArray[cardID].img)
+    if(cardChoosen.length===2){
+       setTimeout(ckeckMatch,500)
+    }
 
 }
